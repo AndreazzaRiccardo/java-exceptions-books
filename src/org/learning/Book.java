0b583391cc9b8dd94a9bb84fa21aacec18fd7659
Riccardo Andreazza <riccardo.andreazza90@gmail.com>
2024-05-21 15:51:23 +0200

    first commit

--- src/org/learning/Book.java
+++ src/org/learning/Book.java
@@ -1,2 +1,72 @@
-package org.learning;public class Book {
+package org.learning;
+
+public class Book {
+    private String title;
+    private String author;
+    private String publisher;
+    private int nPage;
+
+    public Book(String title, String author, String publisher, int nPage) {
+        this.title = validateString(title, "Title");
+        this.author = validateString(author, "Author");
+        this.publisher = validateString(publisher, "Publisher");
+        this.nPage = validateNumber(nPage);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = validateString(title, "Title");
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = validateString(author, "Author");
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = validateString(publisher, "Publisher");
+    }
+
+    public int getnPage() {
+        return nPage;
+    }
+
+    public void setnPage(int nPage) {
+        this.nPage = validateNumber(nPage);
+    }
+
+    protected String validateString(String string, String field) throws IllegalArgumentException {
+        String msg = field + " field cannot be empty";
+        if(string.isEmpty()){
+            throw new IllegalArgumentException(msg);
+        } else {
+            return string;
+        }
+    }
+
+    protected int validateNumber(int number) throws IllegalArgumentException {
+        if(number <= 0){
+            throw new IllegalArgumentException("The number of pages must be greater than zero");
+        } else {
+            return number;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + " - " +
+                "Author: " + author + " - " +
+                "Publisher: " + publisher + " - " +
+                "Number of pages: " + nPage + "\n";
+    }
 }
--- src/org/learning/Main.java
+++ src/org/learning/Main.java
@@ -1,2 +1,83 @@
-package org.learning;public class Main {
+package org.learning;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scann = new Scanner(System.in);
+        File file = new File("./resources/books.txt");
+
+        int nBooks = 0;
+        while (true) {
+            System.out.print("How many books do you want to enter? ");
+            try {
+                nBooks = Integer.parseInt(scann.nextLine());
+                if (nBooks >= 1) {
+                    break;
+                } else {
+                    System.out.println("Enter a number greater than zero.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        Book[] library = new Book[nBooks];
+
+        try {
+            for (int i = 0; i < library.length; i++) {
+                System.out.println("********************************************************");
+                System.out.println("Book number " + (i+1));
+                System.out.print("Insert title: ");
+                String title = scann.nextLine();
+                System.out.print("Insert author: ");
+                String author = scann.nextLine();
+                System.out.print("Insert publisher: ");
+                String publisher = scann.nextLine();
+                System.out.print("Numbers of page: ");
+                int nPage = 0;
+                while (true) {
+                    System.out.print("Number of pages: ");
+                    try {
+                        nPage = Integer.parseInt(scann.nextLine());
+                        break;
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid input. Please enter a valid number of pages.");
+                    }
+                }
+                library[i] = new Book(title, author, publisher, nPage);
+                if(file.exists()){
+                    writeToFile(file, library[i].toString());
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                scann = new Scanner(file);
+                while (scann.hasNextLine()) {
+                    String line = scann.nextLine();
+                    System.out.println(line);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("Cannot read to file");
+            }
+            scann.close();
+        }
+
+    }
+
+    private static void writeToFile(File file, String string) {
+        try(FileWriter fw = new FileWriter(file, true)){
+            fw.write(string);
+            fw.write("********************************************************\n");
+        } catch (IOException e) {
+            System.out.println("Cannot write to file");
+        }
+    }
 }
